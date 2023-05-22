5b75698e74dc0752923d4ae9752bab418a55cafa
twarixx <97103370@st.deltion.nl>
2023-05-22 11:28:32 +0200

    Added auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,44 @@
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+    Navigate,
+    Outlet,
+    RouterProvider,
+    createBrowserRouter,
+} from "react-router-dom";
 import { DashboardPage } from "./pages/DashboardPage";
 import { LoginPage } from "./pages/LoginPage";
 import { UnknownPage } from "./pages/UnknownPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+    const { currentUser } = useContext(AuthContext);
     const queryClient = new QueryClient();
+
+    const LoggedIn = ({ children }) => {
+        if (!currentUser) {
+            return <Navigate to="/login" />;
+        }
+
+        return children;
+    };
+
+    const LoggedOut = ({ children }) => {
+        if (currentUser) {
+            return <Navigate to="/" />;
+        }
+
+        return children;
+    };
 
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <Outlet />,
+            element: (
+                <LoggedIn>
+                    <Outlet />
+                </LoggedIn>
+            ),
             children: [
                 {
                     path: "/",
@@ -20,7 +48,11 @@ function App() {
         },
         {
             path: "/",
-            element: <Outlet />,
+            element: (
+                <LoggedOut>
+                    <Outlet />
+                </LoggedOut>
+            ),
             children: [
                 {
                     path: "/login",
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,3 @@
 export const LoginPage = () => {
-    return (
-        
-    )
-}
+    return <p>Login Page</p>;
+};
