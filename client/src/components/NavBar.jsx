3579ee7581c2290cf7152f64ef1905b72b38eadb
twarixx <97103370@st.deltion.nl>
2023-05-23 12:40:03 +0200

    Fixed the user dropdown

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,9 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Link } from "react-router-dom";
+import { UserDropdown } from "./dropdowns/UserDropdown";
 
 export const NavBar = () => {
-    const { currentUser, logout } = useContext(AuthContext);
-
-    const handleLogout = (event) => {
-        event.preventDefault();
-
-        logout();
-        navigate("/login");
-        toaster.success("Successfully signed out!", {
-            hasCloseButton: true,
-            duration: 5,
-            id: "logout-successful",
-        });
-    };
-
     return (
         <div className="w-full h-16 bg-black bg-opacity-40 flex items-center p-6 justify-between">
             <Link to="/">
@@ -24,9 +11,8 @@ export const NavBar = () => {
                     PASSWORD VAULT
                 </p>
             </Link>
-            <p onClick={handleLogout} className="text-lg hover:cursor-pointer">
-                {currentUser.username}
-            </p>
+
+            <UserDropdown />
         </div>
     );
 };
