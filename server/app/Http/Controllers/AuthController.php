a7b6f8240d677bff6394d1fd115595244f7ec250
ludotielbeke <ludotielbeke@gmail.com>
2023-05-23 14:25:52 +0200

    added some validation to the requiest

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rules\Password;
 use App\Models\Role;
 
