454cbeb94767284b07fe76a5263a7c176261bf2c
twarixx <97103370@st.deltion.nl>
2023-05-23 12:51:36 +0200

    Added search bar too

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,17 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserDropdown } from "./dropdowns/UserDropdown";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export const NavBar = () => {
+    const [search, setSearch] = useState("");
+
+    const submitSearch = (event) => {
+        event.preventDefault();
+
+        // TODO: Just add your search functionality here!
+    };
+
     return (
         <div className="w-full h-16 bg-black bg-opacity-40 flex items-center p-6 justify-between">
             <Link to="/">
@@ -11,6 +19,19 @@ export const NavBar = () => {
                     PASSWORD VAULT
                 </p>
             </Link>
+
+            <div className="w-[50%]">
+                <form onSubmit={submitSearch}>
+                    <input
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="w-full bg-stone-500 text-white placeholder-white pl-9 relative px-2 py-1 rounded outline outline-stone-600 focus:outline-sky-500 hover:outline-sky-500 ring-0"
+                        type="text"
+                        placeholder="Search your vault"
+                    />
+                </form>
+                <AiOutlineSearch className="absolute top-5 ml-1.5" size={24} />
+            </div>
 
             <UserDropdown />
         </div>
