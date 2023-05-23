80ecf9d71dff819220f4855a06dc15d9442a2ce3
twarixx <97103370@st.deltion.nl>
2023-05-23 11:16:47 +0200

    Made base skeleton

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,16 +1,25 @@
 import { Outlet } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { NavBar } from "../components/NavBar";
 
 export const MainLayout = () => {
     return (
-        <div className="h-[100vh]">
-            <div className="h-52">
+        <div className="h-[100vh] w-full text-white">
+            <div className="h-64 w-full relative">
                 <img
-                    className="object-cover object-center w-full h-full"
-                    src="/images/dashboard-background.jpg"
-                    alt="Background Image"
+                    className="h-full w-full object-center object-cover"
+                    src="https://images.unsplash.com/photo-1516663235285-845fac339ca7"
+                    height="16rem"
                 />
+
+                <div className="absolute top-0 w-full">
+                    <NavBar />
+                </div>
+
+                <div className="w-full opacity-100 flex -mt-7">
+                    <Outlet />
+                </div>
             </div>
-            <Outlet />
         </div>
     );
 };
