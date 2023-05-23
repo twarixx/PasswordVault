3c93c53323c4accc5c65263425be411ce27325a3
ludotielbeke <ludotielbeke@gmail.com>
2023-05-23 14:07:30 +0200

    forgot the default value thing in the migration

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role');
-            $table->string('bank');
+            $table->string('bank')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
