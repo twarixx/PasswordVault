cd4741cfbed517fc003865a46e8301d78a36de15
twarixx <97103370@st.deltion.nl>
2023-05-24 14:43:12 +0200

    Fixed categories on the dashboard

--- client/src/components/CategoryList.jsx
+++ client/src/components/CategoryList.jsx
@@ -1,12 +1,32 @@
+import { useContext } from "react";
+import { load } from "../axios";
 import { Category } from "./Category";
+import { AuthContext } from "../context/AuthContext";
 
 export const CategoryList = () => {
+    const { currentUser } = useContext(AuthContext);
+
+    const { data, isLoading, error } = load(
+        ["categories", currentUser.username],
+        `/categories`
+    );
+
+    if (isLoading) return "Loading...";
+    if (error) return <UnknownPage />;
+
+    console.log(data);
+
     return (
         <div className="bg-stone-600 w-full rounded">
-            <Category name="Socials" />
-            <Category name="Games" />
-            <Category name="Work" />
-            <Category name="Super Secret" />
+            {data.map((category) => {
+                return (
+                    <Category
+                        key={category.id}
+                        name={category.name}
+                        id={category.id}
+                    />
+                );
+            })}
         </div>
     );
 };
--- client/src/components/PasswordOverview.jsx
+++ client/src/components/PasswordOverview.jsx
@@ -1,13 +1,7 @@
-
-import { load } from "../axios";
-import { AuthContext } from "../context/AuthContext";
-import { useContext, useState } from "react";
-import { UnknownPage } from "../pages/UnknownPage";
 import { FaEye } from "react-icons/fa";
 
-
 export const PasswordOverview = (data) => {
-    return data.data.map(item => (
+    return data.data.map((item) => (
         <div className="w-full flex justify-between items-center bg-stone-600 border-b-2 border-b-stone-700 px-4 py-3">
             <div className="flex justify-between items-center w-full">
                 <div className="flex">
@@ -18,10 +12,13 @@ export const PasswordOverview = (data) => {
                     </ul>
                 </div>
                 <div className="flex flex-row  gap-5">
-                    <p className=" bg-blue-500 rounded w-full flex items-center justify-center p-4" > Social Media </p>
+                    <p className=" bg-blue-500 rounded w-full flex items-center justify-center p-4">
+                        {" "}
+                        Social Media{" "}
+                    </p>
                     <FaEye className="rounded text-color-white" size={50} />
                 </div>
             </div>
-        </div>)
-    )
-}
+        </div>
+    ));
+};
