c095cdb48a10211c6e8ec32701084730be0521d8
twarixx <97103370@st.deltion.nl>
2023-05-26 11:04:28 +0200

    Validation on master password

--- client/src/components/CategoryList.jsx
+++ client/src/components/CategoryList.jsx
@@ -3,6 +3,7 @@ import { load } from "../axios";
 import { Category } from "./Category";
 import { AuthContext } from "../context/AuthContext";
 import { UnknownPage } from "../pages/UnknownPage";
+import { Link } from "react-router-dom";
 
 export const CategoryList = () => {
     const { currentUser } = useContext(AuthContext);
@@ -17,6 +18,12 @@ export const CategoryList = () => {
 
     return (
         <div className="bg-stone-600 w-full rounded">
+            <Link to={`/`}>
+                <div className="w-full flex justify-between items-center bg-stone-600 border-b-2 border-b-stone-700 px-4 py-6">
+                    <h1 className="text-lg font-semibold">Home</h1>
+                </div>
+            </Link>
+
             {data.map((category) => {
                 return (
                     <Category
--- client/src/components/dialogs/MasterPasswordDialog.jsx
+++ client/src/components/dialogs/MasterPasswordDialog.jsx
@@ -1,14 +1,54 @@
-import { Dialog } from '@headlessui/react'
-import { useContext, useState } from 'react'
+import { Dialog } from "@headlessui/react";
+import { useContext, useState } from "react";
 import { Transition } from "@headlessui/react";
 import { useNavigate } from "react-router-dom";
-import { MasterPasswordContext } from '../../context/MasterPasswordContext';
+import { MasterPasswordContext } from "../../context/MasterPasswordContext";
+import { useMutation } from "@tanstack/react-query";
+import { PulseLoader } from "react-spinners";
+import { makeRequest } from "../../axios";
+import { toaster } from "evergreen-ui";
 
 export const MasterPasswordDialog = ({ isOpen, onClose, destination }) => {
     const [masterPassword, setMasterPassword] = useState("");
+    const [saving, setSaving] = useState(false);
     const navigate = useNavigate();
     const { updateMasterPassword } = useContext(MasterPasswordContext);
 
+    const mutation = useMutation(
+        (data) => {
+            return makeRequest.post(`/masterpassword`, {
+                masterpassword: masterPassword,
+            });
+        },
+        {
+            onSuccess: (data) => {
+                setSaving(false);
+
+                updateMasterPassword(masterPassword);
+                navigate(destination);
+            },
+            onError: (error) => {
+                setSaving(false);
+                setMasterPassword("");
+
+                toaster.danger(error.response.data.message, {
+                    hasCloseButton: true,
+                    duration: 3,
+                    id: "password-failed",
+                });
+            },
+        }
+    );
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (!masterPassword) return;
+        if (saving) return;
+
+        setSaving(true);
+        mutation.mutate();
+    };
 
     return (
         <Transition appear show={isOpen}>
@@ -41,31 +81,35 @@ export const MasterPasswordDialog = ({ isOpen, onClose, destination }) => {
                                 >
                                     Enter Master Password
                                 </Dialog.Title>
-                                <div className="mt-2">
-                                    <input
-                                        value={masterPassword}
-                                        onChange={(e) =>
-                                            setMasterPassword(e.target.value)
-                                        }
-                                        id="masterPassword"
-                                        name="masterPassword"
-                                        type="password"
-                                        className="bg-stone-500 px-2 py-1.5 rounded placeholder-white text-white border w-[250px] border-stone-600 hover:border-sky-500 focus:border-sky-500 ring-0 outline-none"
-                                    ></input>
-                                </div>
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mt-2">
+                                        <input
+                                            value={masterPassword}
+                                            onChange={(e) =>
+                                                setMasterPassword(
+                                                    e.target.value
+                                                )
+                                            }
+                                            id="masterPassword"
+                                            name="masterPassword"
+                                            type="password"
+                                            className="bg-stone-500 px-2 py-1.5 rounded placeholder-white text-white border w-[250px] border-stone-600 hover:border-sky-500 focus:border-sky-500 ring-0 outline-none"
+                                        ></input>
+                                    </div>
 
-                                <div className="mt-4">
-                                    <button
-                                        type="button"
-                                        className="bg-sky-500 h-12  flex justify-center w-[250px] items-center hover:bg-sky-600 focus:bg-sky-600 transition rounded   px-4 mt-5 py-2"
-                                        onClick={() => {
-                                            navigate(destination);
-                                            updateMasterPassword(masterPassword);
-                                        }}
-                                    >
-                                        Authenticate !
-                                    </button>
-                                </div>
+                                    <div className="mt-4">
+                                        <button
+                                            type="submit"
+                                            className="bg-sky-500 h-12 text-white flex justify-center w-[250px] items-center hover:bg-sky-600 focus:bg-sky-600 transition rounded   px-4 mt-5 py-2"
+                                        >
+                                            {saving ? (
+                                                <PulseLoader color="white" />
+                                            ) : (
+                                                "Authenticate"
+                                            )}
+                                        </button>
+                                    </div>
+                                </form>
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
