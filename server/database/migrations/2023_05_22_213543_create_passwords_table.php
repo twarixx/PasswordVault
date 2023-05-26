53f17619e402bdd43164d71f0bea4d2d567d18f2
Lucaspinder <80094024+Lucaspinder@users.noreply.github.com>
2023-05-26 09:19:58 +0200

    merge with features/passwordedit 3

--- server/database/migrations/2023_05_22_213543_create_passwords_table.php
+++ server/database/migrations/2023_05_22_213543_create_passwords_table.php
@@ -14,17 +14,10 @@ return new class extends Migration
     {
         Schema::create('passwords', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
-            $table->foreignid(User::class);
-            $table->string('website');
-            $table->string('username');
-            $table->string('password');
-=======
             $table->foreignid('user_id')->constrained('users');
             $table->string('website');
             $table->string('username');
             $table->mediumText('password');
->>>>>>> def6cfa4f9dbd01185f9c809a98d2deb38cf42c4
             $table->string('category')->nullable();
             $table->timestamps();
         });
--- server/routes/api.php
+++ server/routes/api.php
@@ -4,12 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UpgradeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\API\CategoryController;
->>>>>>> def6cfa4f9dbd01185f9c809a98d2deb38cf42c4
 use App\Http\Controllers\API\PasswordController;
-use App\Http\Controllers\API\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,11 +28,8 @@ Route::middleware('auth')->put('/passwords/{password}', [PasswordController::cla
 Route::post('/passwords', [PasswordController::class, 'store']);
 Route::get('/passwords', [PasswordController::class, 'index']);
 Route::delete('/passwords/{password}', [PasswordController::class, 'destroy']);
-<<<<<<< HEAD
-=======
 
 Route::post('/search', [PasswordController::class, 'search']);
->>>>>>> def6cfa4f9dbd01185f9c809a98d2deb38cf42c4
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
@@ -55,15 +48,12 @@ Route::middleware('auth')->get('/testauth', function () {
     return response(Auth::user());
 });
 
-<<<<<<< HEAD
-=======
 Route::middleware('auth')->get('/passwords', [PasswordController::class, 'index']);
 Route::middleware('auth')->post('/passwords', [PasswordController::class, 'store']);
 Route::middleware('auth')->get('/passwords/{password}', [PasswordController::class, 'show']);
 Route::middleware('auth')->put('/passwords/', [PasswordController::class, 'update']);
 Route::middleware('auth')->delete('/passwords/{password}', [PasswordController::class, 'destroy']);
 
->>>>>>> def6cfa4f9dbd01185f9c809a98d2deb38cf42c4
 Route::middleware('auth')->post('/upgrade', [UpgradeController::class, 'upgrade']);
 Route::middleware('auth')->post('/downgrade', [UpgradeController::class, 'downgrade']);
 
